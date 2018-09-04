6825528092bd3fe3e331f293987ee91ca05cb727
João Harbs <harbsprog@gmail.com>
2018-09-04 12:53:38 -0300

    Comecando a implementar um metodo que imprime o tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -8,6 +8,8 @@ namespace xadrez_console {
 
             Tabuleiro tab = new Tabuleiro(8, 8);
 
+            Tela.imprimirTabuleiro(tab);
+
             Console.ReadLine();
 
         }
--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -12,5 +12,9 @@
             pecas = new Peca[linhas, colunas];
         }
 
+        public Peca peca(int linha, int coluna) {
+            return pecas[linha, coluna];
+        }
+
     }
 }
