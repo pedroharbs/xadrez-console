2b6c564542a817a7324a2c2405d8ca1ef72a3191
João Harbs <harbsprog@gmail.com>
2018-09-04 13:11:28 -0300

    Comecando a colocar pecas no tabuleiro

---
+++ xadrez-console/xadrez/Peao.cs
@@ -0,0 +1,15 @@
+﻿using tabuleiro;
+
+namespace xadrez {
+
+    class Peao : Peca{
+
+        public Peao(Tabuleiro tab, Cor cor) : base(tab, cor) {
+        }
+
+        public override string ToString() {
+            return "P";
+        }
+
+    }
+}
