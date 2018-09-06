53217e7f2bf6e7cd2c275c165947e9d6f8d543ca
João Harbs <harbsprog@gmail.com>
2018-09-06 12:59:27 -0300

    Correcao: retirados caracteres invalidos na posicao

--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -43,11 +43,33 @@ namespace xadrez_console {
 
 
         public static PosicaoXadrez lerPosicaoXadrez() {
-            string s = Console.ReadLine();
-            char coluna = s[0];
-            int linha = int.Parse(s[1] + " ");
+            string[] posicoesA = new string[] { "a1", "a2", "a3", "a4", "a5", "a6", "a7", "a8" };
+            string[] posicoesB = new string[] { "b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8" };
+            string[] posicoesC = new string[] { "c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8" };
+            string[] posicoesD = new string[] { "d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8" };
+            string[] posicoesE = new string[] { "e1", "e2", "e3", "e4", "e5", "e6", "e7", "e8" };
+            string[] posicoesF = new string[] { "f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8" };
+            string[] posicoesG = new string[] { "g1", "g2", "g3", "g4", "g5", "g6", "g7", "g8" };
+            string[] posicoesH = new string[] { "h1", "h2", "h3", "h4", "h5", "h6", "h7", "h8" };
 
-            return new PosicaoXadrez(coluna, linha);
+            string s = Console.ReadLine();
+            if (s == "" || s.Contains("'") || s.Contains("!") || s.Contains("@") || s.Contains("¹") 
+                || s.Contains("²") || s.Contains("³") || s.Contains("£") || s.Contains("#") || s.Contains("$")
+                 || s.Contains("¢") || s.Contains("%") || s.Contains("¨") || s.Contains("¬") || s.Contains("&")
+                  || s.Contains("*") || s.Contains("(") || s.Contains(")") || s.Contains("_") || s.Contains("-")
+                   || s.Contains("§") || s.Contains("+") || s.Contains("=") || s.Contains("|") || s.Contains("'\'")
+                    || s.Contains("<") || s.Contains(",") || s.Contains(">") || s.Contains(".") || s.Contains(":")
+                     || s.Contains(";") || s.Contains("ç") || s.Contains("Ç") || s.Contains("^") || s.Contains("~")
+                      || s.Contains("}") || s.Contains("]") || s.Contains("º") || s.Contains("{") || s.Contains("[")
+                       || s.Contains("ª") || s.Contains("´") || s.Contains("`") || s.Contains("/") || s.Contains("°")
+                        || s.Contains("?")) {
+                throw new TabuleiroException("Digite Uma Posição Válida!");
+            } else {
+                char coluna = s[0];
+                int linha = int.Parse(s[1] + " ");
+                return new PosicaoXadrez(coluna, linha);
+
+            }
         }
 
         public static void imprimirPeca(Peca peca) {
